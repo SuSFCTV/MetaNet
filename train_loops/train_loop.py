8a7e1675547bf2e4b507128f7db381a2c82b2d16
Suslov Mikhail <misha.suslov.2016@mail.ru>
2022-02-25 22:38:45 +0300

    feat: end parallel model

--- train_loops/train_loop.py
+++ train_loops/train_loop.py
@@ -13,6 +13,66 @@ import matplotlib.pyplot as plt
 import time
 
 
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, x):
+        return x
+
+
+class ParallelNet(nn.Module):
+    def __init__(self, num_classes: int = 2, dropout: float = 0.5) -> None:
+        super().__init__()
+        self.model_pretrained = models.alexnet(pretrained=True)
+        self.model_pretrained.classifier = Identity()  # костыль
+        for param in self.model_pretrained.parameters():  # замораживаю веса предобученной модели
+            param.requires_grad = False
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x_1: torch.Tensor, x_2: torch.Tensor) -> torch.Tensor:
+        # print("x_2 size before learning:", x_2.size())
+        x_2 = self.model_pretrained(x_2)
+        print("x_2 size after learning:", x_2.size())
+        print("x_1 size before learning", x_1.size())
+        x_1 = self.features(x_1)
+        x_1 = self.avgpool(x_1)
+        print("x_1 size after CNN ", x_1.size())
+        x_1 = torch.flatten(x_1, 1)
+        print("x_1 size after flatten ", x_1.size())
+        x_1 = torch.cat((x_1, x_2), 0)
+        print("x_1 size after concat ", x_1.size())
+        x_1 = self.classifier(x_1)
+        print("x_1 size after classifier:", x_1.size())
+        x_1 = x_1.view(4, 4)
+        print("x_1 size after view:", x_1.size())
+        return x_1
+
+
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
@@ -94,3 +154,41 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     # загрузим лучшие веса модели
     model.load_state_dict(best_model_wts)
     return model, losses
+
+
+if __name__ == "__main__":
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.RandomResizedCrop(244),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'val': transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(244),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
+
+    data_dir = './data'
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                              data_transforms[x])
+                      for x in ['train', 'val']}
+    # специальный класс для загрузки данных в виде батчей
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                                  shuffle=True, num_workers=2)
+                   for x in ['train', 'val']}
+    dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+    class_names = image_datasets['train'].classes
+
+    use_gpu = torch.cuda.is_available()
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer_ft = optim.Adam(ParallelNet().parameters(), lr=1e-4)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+    parallel_net = ParallelNet()
+
+    model_parallel, losses = train_model(parallel_net, loss_fn, optimizer_ft, exp_lr_scheduler, num_epochs=25)
