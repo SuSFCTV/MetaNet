af316241004ff975f48ee0600c5235194e0f224d
Suslov Mikhail <misha.suslov.2016@mail.ru>
2022-02-25 11:14:32 +0300

    feat: add train loop

---
+++ train_loops/train_loop.py
@@ -0,0 +1,96 @@
+import os
+from tqdm.autonotebook import tqdm, trange
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = model.state_dict()
+    best_acc = 0.0
+
+    # Ваш код здесь
+    losses = {'train': [], "val": []}
+
+    pbar = trange(num_epochs, desc="Epoch:")
+
+    for epoch in pbar:
+
+        # каждя эпоха имеет обучающую и тестовую стадии
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train(True)  # установаить модель в режим обучения
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # итерируемся по батчам
+            for data in tqdm(dataloaders[phase], leave=False, desc=f"{phase} iter:"):
+                # получаем картинки и метки
+                inputs, labels = data
+
+                # оборачиваем в переменные
+                if use_gpu:
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
+                else:
+                    inputs, labels = inputs, labels
+
+                # инициализируем градиенты параметров
+                if phase == "train":
+                    optimizer.zero_grad()
+
+                # forward pass
+                if phase == "eval":
+                    with torch.no_grad():
+                        outputs = model(inputs, inputs)
+                else:
+                    outputs = model(inputs, inputs)
+                preds = torch.argmax(outputs, -1)
+                loss = criterion(outputs, labels)
+
+                # backward pass + оптимизируем только если это стадия обучения
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                # статистика
+                running_loss += loss.item()
+                running_corrects += int(torch.sum(preds == labels.data))
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects / dataset_sizes[phase]
+
+            # Ваш код здесь
+            losses[phase].append(epoch_loss)
+
+            pbar.set_description('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc
+            ))
+
+            # если достиглось лучшее качество, то запомним веса модели
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = model.state_dict()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # загрузим лучшие веса модели
+    model.load_state_dict(best_model_wts)
+    return model, losses
